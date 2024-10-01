75db70a564adb58e53f0e055009a5e30f19b42b8
David Stroud <>
2024-10-01 09:26:07 -0500

    Add DoubleCounter class

--- src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -9,6 +9,7 @@ import edu.grinnell.csc207.util.BasicCounter;
 import edu.grinnell.csc207.util.Counter;
 import edu.grinnell.csc207.util.Tally;
 import edu.grinnell.csc207.util.DecrementableCounter;
+import edu.grinnell.csc207.util.DoubleCounter;
 import edu.grinnell.csc207.util.NamedCounter;
 
 /**
@@ -27,7 +28,7 @@ public class CounterExperiments {
 
     // Set up some counters
         Counter alpha = new NamedCounter("alfa", 0);
-    Counter beta = new BasicCounter(123);
+        Counter beta = new DoubleCounter(123);
     Counter gamma = new DecrementableCounter(-5);
 
     // Print original values
